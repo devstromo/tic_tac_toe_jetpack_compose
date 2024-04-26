8c096c17210f259a99e9fa08498afd34dfccc825
alemoles <alemolerio@gmail.com>
2024-04-25 22:05:52 -0300

    Advanced Tic Tac Toe Jetpack Compose | create start set up

---
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/BoardScreen.kt
@@ -0,0 +1,10 @@
+package com.devstromo.advancedtictactoe.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BoardScreen() {
+    Text(text = "Board Screen")
+    
+}
