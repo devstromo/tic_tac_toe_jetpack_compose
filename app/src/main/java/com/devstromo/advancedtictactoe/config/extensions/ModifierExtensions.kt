5a8095b19c22b0ee84f6662e379247dc387b3ad8
alemoles <alemolerio@gmail.com>
2024-04-30 20:55:18 -0300

    Advanced Tic Tac Toe Jetpack Compose | add conditional border modifier

--- app/src/main/java/com/devstromo/advancedtictactoe/config/extensions/ModifierExtensions.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/config/extensions/ModifierExtensions.kt
@@ -2,7 +2,5 @@ package com.devstromo.advancedtictactoe.config.extensions
 
 import androidx.compose.ui.Modifier
 
-class ModifierExtensions {
-    fun Modifier.modifyIf(condition: Boolean, modify: Modifier.() -> Modifier) =
-        if (condition) modify() else this
-}
+fun Modifier.modifyIf(condition: Boolean, modify: Modifier.() -> Modifier) =
+    if (condition) modify() else this
--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/PlayerMarker.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/PlayerMarker.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import com.devstromo.advancedtictactoe.config.extensions.modifyIf
 import com.devstromo.advancedtictactoe.ui.theme.AdvancedTicTacToeTheme
 import com.devstromo.advancedtictactoe.ui.theme.kMainDarkThemeColor
 
@@ -41,11 +42,13 @@ fun PlayerMarker(
             .background(
                 color = kMainDarkThemeColor
             )
-            .border(
-                width = 1.dp,
-                color = Color.White,
-                shape,
-            )
+            .modifyIf(state.isSelected) {
+                border(
+                    width = 1.dp,
+                    color = Color.White,
+                    shape,
+                )
+            }
     ) {
         Box {
             Box(
