31308cbb1e42b9203c13ab805ca24e2231af457e
alemoles <alemolerio@gmail.com>
2024-04-30 23:08:47 -0300

    Advanced Tic Tac Toe Jetpack Compose | enable edge to edge mode

--- app/src/main/java/com/devstromo/advancedtictactoe/MainActivity.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devstromo.advancedtictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: GameViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         startKoin {
             androidLogger()
