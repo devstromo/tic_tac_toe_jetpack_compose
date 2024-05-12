46477fb8a7bbc2baff8be169335bfac4f9893c8c
alemoles <alemolerio@gmail.com>
2024-05-12 00:52:42 -0300

    Advanced Tic Tac Toe Jetpack Compose | code refactor

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -2,12 +2,9 @@ package com.devstromo.advancedtictactoe.presentation.initial
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.devstromo.advancedtictactoe.navigation.Screen
 import com.devstromo.advancedtictactoe.presentation.components.CustomButton
