da9c6cb9adb4717a237b884aa816ff1271f8a986
alemoles <alemolerio@gmail.com>
2024-05-12 01:05:01 -0300

    Advanced Tic Tac Toe Jetpack Compose | add horizontal padding to initial screen

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -3,8 +3,10 @@ package com.devstromo.advancedtictactoe.presentation.initial
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.devstromo.advancedtictactoe.navigation.Screen
 import com.devstromo.advancedtictactoe.presentation.components.CustomButton
@@ -16,7 +18,10 @@ fun InitialScreen(
 ) {
     Column(
         modifier = modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(
+                horizontal = 45.dp,
+            ),
         verticalArrangement = Arrangement.Center
     ) {
         CustomButton(
