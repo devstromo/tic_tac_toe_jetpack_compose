7f7a0fbb704eb7adcad7e1cc37a7d6f06c45929c
alemoles <alemolerio@gmail.com>
2024-05-12 00:42:26 -0300

    Advanced Tic Tac Toe Jetpack Compose | create initial screen

---
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -0,0 +1,32 @@
+package com.devstromo.advancedtictactoe.presentation.initial
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.devstromo.advancedtictactoe.navigation.Screen
+import com.devstromo.advancedtictactoe.presentation.components.CustomButton
+
+@Composable
+fun InitialScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        CustomButton(
+            text = "Play",
+            onClick = {
+                navController.navigate(route = Screen.Game.route)
+            },
+        )
+    }
+}
