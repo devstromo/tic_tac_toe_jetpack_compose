1c11e3bc70ccadad8706e59d82b88127904c5b9e
alemoles <alemolerio@gmail.com>
2024-05-13 21:50:53 -0300

    Advanced Tic Tac Toe Jetpack Compose | add app title in initial screen

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -2,10 +2,16 @@ package com.devstromo.advancedtictactoe.presentation.initial
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.devstromo.advancedtictactoe.navigation.Screen
@@ -16,14 +22,25 @@ fun InitialScreen(
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
+    val typo = MaterialTheme.typography
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(
                 horizontal = 45.dp,
+                vertical = 20.dp
             ),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Tic-Tac-Toe",
+            style = typo.titleLarge.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(modifier = Modifier.weight(1f))
         CustomButton(
             text = "Play",
             onClick = {
