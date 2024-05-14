3d0f0e811f47a1b9c1114f4ca6861452cc44b95a
alemoles <alemolerio@gmail.com>
2024-05-13 22:27:02 -0300

    Advanced Tic Tac Toe Jetpack Compose | update back button design in rules screen

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
@@ -1,8 +1,6 @@
 package com.devstromo.advancedtictactoe.presentation.rules
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,16 +11,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
