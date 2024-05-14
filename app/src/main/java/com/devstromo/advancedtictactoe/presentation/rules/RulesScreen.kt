c060174102c94ea0bb83e8f593c72c265bba4ffa
alemoles <alemolerio@gmail.com>
2024-05-13 22:26:50 -0300

    Advanced Tic Tac Toe Jetpack Compose | update back button design in rules screen

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
@@ -1,15 +1,25 @@
 package com.devstromo.advancedtictactoe.presentation.rules
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,19 +31,31 @@ fun RulesScreen(
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
+    val color = MaterialTheme.colorScheme
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(10.dp)
                 .align(Alignment.TopStart)
         ) {
-            IconButton(onClick = { navController.popBackStack() }) {
+            IconButton(
+                modifier = Modifier
+                    .height(60.dp)
+                    .width(60.dp)
+                    .background(
+                        color = color.primary,
+                        shape = RoundedCornerShape(25)
+                    ),
+
+                onClick = { navController.popBackStack() },
+            ) {
                 Icon(
-                    Icons.Filled.ArrowBack,
+                    Icons.Rounded.ArrowBack,
                     contentDescription = "Favorite",
-                    tint = Color.Red,
+                    tint = color.secondary,
                     modifier = Modifier
                         .size(48.dp)
                 )
