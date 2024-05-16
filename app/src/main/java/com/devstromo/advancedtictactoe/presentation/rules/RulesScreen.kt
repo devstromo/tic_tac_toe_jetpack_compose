d43e7f529b4505040ef2641268300e8f7312c40c
alemoles <alemolerio@gmail.com>
2024-05-15 22:35:07 -0300

    Advanced Tic Tac Toe Jetpack Compose | code refactor

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/rules/RulesScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,7 +22,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
