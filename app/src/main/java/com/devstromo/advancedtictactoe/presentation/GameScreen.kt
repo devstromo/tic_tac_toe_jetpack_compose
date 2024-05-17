14037b7369d7a3ff4ff1e02abbc55df6a20cab69
alemoles <alemolerio@gmail.com>
2024-05-16 22:44:08 -0300

    Advanced Tic Tac Toe Jetpack Compose | add end alert dialog

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/GameScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/GameScreen.kt
@@ -17,9 +17,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,6 +52,40 @@ fun GameScreen(
     val typo = MaterialTheme.typography
     val player1Moves = viewModel.countPlayer1Moves()
     val player2Moves = viewModel.countPlayer2Moves()
+    // State to manage dialog visibility
+    val showDialog = remember { mutableStateOf(false) }
+
+    // Trigger the dialog when the game ends
+    LaunchedEffect(state.isGameOver) {
+        if (state.isGameOver) {
+            showDialog.value = true
+        }
+    }
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = {
+                Text(text = "Game Over")
+            },
+            text = {
+                Text(
+                    text = if (viewModel.checkForWinner(state.board)) {
+                        "Player ${state.currentPlayer} wins!"
+                    } else {
+                        "It's a draw!"
+                    }
+                )
+            },
+            confirmButton = {
+                Button(onClick = {
+                    showDialog.value = false
+                    viewModel.resetGame()
+                }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
