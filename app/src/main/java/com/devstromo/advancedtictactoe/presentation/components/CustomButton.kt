30125fa48e1972fffdf43dce016bdc48ba54d25e
alemoles <alemolerio@gmail.com>
2024-05-21 21:37:06 -0300

    Advanced Tic Tac Toe Jetpack Compose | fix custom button color

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/CustomButton.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/CustomButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.devstromo.advancedtictactoe.ui.theme.kSecondaryDarkThemeColor
 
 @Composable
 fun CustomButton(
@@ -26,7 +28,10 @@ fun CustomButton(
             .fillMaxWidth(),
         shape = RoundedCornerShape(25),
         onClick = onClick,
-        enabled = isEnable
+        enabled = isEnable,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = kSecondaryDarkThemeColor,
+        )
     ) {
         if (text.isNotBlank()) {
             Text(
