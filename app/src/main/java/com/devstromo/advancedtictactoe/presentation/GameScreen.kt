8051eb6ea86440e7f6339a0db4d84b76a74e8577
alemoles <alemolerio@gmail.com>
2024-05-22 22:18:39 -0300

    Advanced Tic Tac Toe Jetpack Compose | update board ui

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/GameScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/GameScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -41,6 +42,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.devstromo.advancedtictactoe.R
@@ -53,6 +55,7 @@ import com.devstromo.advancedtictactoe.presentation.components.PlayerMarkerType
 import com.devstromo.advancedtictactoe.ui.theme.kPlayerOMarkColor
 import com.devstromo.advancedtictactoe.ui.theme.kPlayerXMarkColor
 import java.util.Locale
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedBoxWithConstraintsScope")
@@ -67,11 +70,13 @@ fun GameScreen(
     val context = LocalContext.current
     val showDialog = remember { mutableStateOf(false) }
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
     LaunchedEffect(state.isGameOver) {
         if (state.isGameOver) {
             showDialog.value = true
         }
     }
+
     if (showDialog.value) {
         AlertDialog(
             onDismissRequest = { showDialog.value = false },
@@ -91,8 +96,7 @@ fun GameScreen(
             },
             confirmButton = {
                 Button(
-                    modifier = Modifier
-                        .fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(25),
                     onClick = {
                         showDialog.value = false
@@ -105,14 +109,13 @@ fun GameScreen(
                             .padding(top = 10.dp),
                         text = "Reset Game",
                         textAlign = TextAlign.Center,
-                        style = typo.bodyLarge.copy(
-                            color = Color.White
-                        )
+                        style = typo.bodyLarge.copy(color = Color.White)
                     )
                 }
             }
         )
     }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -127,9 +130,7 @@ fun GameScreen(
             title = {
                 Text(
                     state.gameMode.name.lowercase(Locale.ROOT).replaceFirstChar {
-                        if (it.isLowerCase()) it.titlecase(
-                            Locale.ROOT
-                        ) else it.toString()
+                        if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString()
                     },
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
@@ -150,8 +151,7 @@ fun GameScreen(
                         Icons.Rounded.Home,
                         contentDescription = "Home",
                         tint = Color.White,
-                        modifier = Modifier
-                            .size(40.dp)
+                        modifier = Modifier.size(40.dp)
                     )
                 }
             },
@@ -189,9 +189,7 @@ fun GameScreen(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(
-                    horizontal = 45.dp,
-                )
+                .padding(horizontal = 45.dp)
         ) {
             CustomButton(
                 text = "Reset Game",
@@ -216,36 +214,37 @@ fun BoardContent(
     viewModel: GameViewModel,
     context: Context
 ) {
-    val color = MaterialTheme.colorScheme
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                horizontal = 45.dp,
-            )
-            .background(
-                color = color.primary,
-                shape = RoundedCornerShape(
-                    15.dp
-                )
-            )
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
     ) {
-        for (row in 0..2) {
-            val positions = List(3) { col ->
-                Pair(row, col)
+        val boardSize = maxWidth * 0.9f
+        val keySize = boardSize / 3
+
+        Column(
+            modifier = Modifier
+                .size(boardSize)
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(15.dp)
+                )
+        ) {
+            for (row in 0..2) {
+                val positions = List(3) { col -> Pair(row, col) }
+                BoardRow(
+                    onItemSelected = { selectedPair ->
+                        Log.i("Board", "Pair $selectedPair")
+                        onItemSelected(selectedPair.first, selectedPair.second)
+                    },
+                    positions = positions,
+                    rowState = boardState[row],
+                    isGameOver = isGameOver,
+                    nextMoveToRemove = nextMoveToRemove,
+                    keySize = keySize,
+                    viewModel = viewModel,
+                    context = context
+                )
             }
-            BoardRow(
-                onItemSelected = { selectedPair ->
-                    Log.i("Board", "Pair $selectedPair")
-                    onItemSelected(selectedPair.first, selectedPair.second)
-                },
-                positions = positions,
-                rowState = boardState[row],
-                isGameOver = isGameOver,
-                nextMoveToRemove = nextMoveToRemove,
-                viewModel = viewModel,
-                context = context
-            )
         }
     }
 }
@@ -257,6 +256,7 @@ fun BoardRow(
     rowState: List<Player?>,
     isGameOver: Boolean,
     nextMoveToRemove: Pair<Int, Int>? = null,
+    keySize: Dp,
     viewModel: GameViewModel,
     context: Context
 ) {
@@ -274,13 +274,11 @@ fun BoardRow(
         positions.forEachIndexed { index, pair ->
             BoardKeyBox(
                 onItemSelected = { onItemSelected(pair) },
-                player = if (rowState[pair.second] == Player.NONE)
-                    null
-                else
-                    rowState[pair.second],
+                player = rowState[pair.second],
                 isClickable = !isGameOver,
                 isNextToRemove = nextMoveToRemove == pair,
                 soundResId = soundResources[index],
+                keySize = keySize,
                 viewModel = viewModel,
                 context = context
             )
@@ -295,6 +293,7 @@ fun BoardKeyBox(
     isClickable: Boolean = true,
     isNextToRemove: Boolean = false,
     soundResId: Int,
+    keySize: Dp,
     viewModel: GameViewModel,
     context: Context
 ) {
@@ -302,12 +301,10 @@ fun BoardKeyBox(
     val alpha = if (isNextToRemove) 0.5f else 1f
     Box(
         modifier = Modifier
-            .width(80.dp)
-            .height(80.dp)
+            .size(keySize)
             .background(
-                color = color.secondary.copy(alpha = alpha), shape = RoundedCornerShape(
-                    10.dp
-                )
+                color = color.secondary.copy(alpha = alpha),
+                shape = RoundedCornerShape(10.dp)
             )
             .clickable(
                 enabled = player == null && isClickable,
@@ -317,13 +314,12 @@ fun BoardKeyBox(
                 },
                 indication = rememberRipple(
                     bounded = true,
-                    radius = 70.dp,
+                    radius = keySize / 2,
                     color = color.primary
                 ),
-                interactionSource = remember {
-                    MutableInteractionSource()
-                }
+                interactionSource = remember { MutableInteractionSource() }
             ),
+        contentAlignment = Alignment.Center
     ) {
         Text(
             text = when (player) {
@@ -331,12 +327,7 @@ fun BoardKeyBox(
                 Player.PLAYER_2 -> "O"
                 else -> ""
             },
-            style = MaterialTheme.typography.displayMedium.copy(
-                textAlign = TextAlign.Center
-            ),
-            modifier = Modifier
-                .padding(top = 20.dp, start = 5.dp)
-                .align(Alignment.Center),
+            style = MaterialTheme.typography.displayMedium.copy(textAlign = TextAlign.Center),
             color = when (player) {
                 Player.PLAYER_1 -> kPlayerXMarkColor
                 Player.PLAYER_2 -> kPlayerOMarkColor
@@ -345,3 +336,295 @@ fun BoardKeyBox(
         )
     }
 }
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@SuppressLint("UnusedBoxWithConstraintsScope")
+//@Composable
+//fun GameScreen(
+//    navController: NavController,
+//    viewModel: GameViewModel,
+//    state: GameUiState,
+//    onItemSelected: (Int, Int) -> Unit,
+//) {
+//    val typo = MaterialTheme.typography
+//    val context = LocalContext.current
+//    val showDialog = remember { mutableStateOf(false) }
+//    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+//    LaunchedEffect(state.isGameOver) {
+//        if (state.isGameOver) {
+//            showDialog.value = true
+//        }
+//    }
+//    if (showDialog.value) {
+//        AlertDialog(
+//            onDismissRequest = { showDialog.value = false },
+//            title = {
+//                Text(text = "Game Over")
+//            },
+//            text = {
+//                Text(
+//                    text = if (viewModel.checkForWinner(state.board)) {
+//                        if (state.currentPlayer == Player.PLAYER_1) {
+//                            "You win!"
+//                        } else "Your opponent wins!"
+//                    } else {
+//                        "It's a draw!"
+//                    }
+//                )
+//            },
+//            confirmButton = {
+//                Button(
+//                    modifier = Modifier
+//                        .fillMaxWidth(),
+//                    shape = RoundedCornerShape(25),
+//                    onClick = {
+//                        showDialog.value = false
+//                        viewModel.resetGame()
+//                    }
+//                ) {
+//                    Text(
+//                        modifier = Modifier
+//                            .align(Alignment.CenterVertically)
+//                            .padding(top = 10.dp),
+//                        text = "Reset Game",
+//                        textAlign = TextAlign.Center,
+//                        style = typo.bodyLarge.copy(
+//                            color = Color.White
+//                        )
+//                    )
+//                }
+//            }
+//        )
+//    }
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(bottom = 30.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        CenterAlignedTopAppBar(
+//            colors = TopAppBarDefaults.topAppBarColors(
+//                containerColor = Color.Transparent,
+//                titleContentColor = Color.White,
+//            ),
+//            title = {
+//                Text(
+//                    state.gameMode.name.lowercase(Locale.ROOT).replaceFirstChar {
+//                        if (it.isLowerCase()) it.titlecase(
+//                            Locale.ROOT
+//                        ) else it.toString()
+//                    },
+//                    maxLines = 1,
+//                    overflow = TextOverflow.Ellipsis
+//                )
+//            },
+//            navigationIcon = {
+//                IconButton(
+//                    modifier = Modifier
+//                        .padding(start = 20.dp)
+//                        .height(45.dp)
+//                        .width(45.dp)
+//                        .background(
+//                            color = Color.Transparent,
+//                        ),
+//                    onClick = { navController.navigate(route = Screen.Initial.route) },
+//                ) {
+//                    Icon(
+//                        Icons.Rounded.Home,
+//                        contentDescription = "Home",
+//                        tint = Color.White,
+//                        modifier = Modifier
+//                            .size(40.dp)
+//                    )
+//                }
+//            },
+//            scrollBehavior = scrollBehavior,
+//        )
+//        Row(
+//            modifier = Modifier.padding(vertical = 10.dp)
+//        ) {
+//            PlayerMarker(
+//                state = PlayerMakerState(
+//                    PlayerMarkerType.X,
+//                    isSelected = state.currentPlayer == Player.PLAYER_1,
+//                    itemsCount = state.player1MoveCount
+//                )
+//            )
+//            Spacer(modifier = Modifier.width(20.dp))
+//            PlayerMarker(
+//                state = PlayerMakerState(
+//                    PlayerMarkerType.O,
+//                    isSelected = state.currentPlayer == Player.PLAYER_2,
+//                    itemsCount = state.player2MoveCount
+//                )
+//            )
+//        }
+//        Spacer(modifier = Modifier.weight(1f))
+//        BoardContent(
+//            onItemSelected,
+//            boardState = state.board,
+//            isGameOver = state.isGameOver,
+//            nextMoveToRemove = state.nextMoveToRemove,
+//            viewModel = viewModel,
+//            context = context
+//        )
+//        Spacer(modifier = Modifier.weight(1f))
+//        Column(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(
+//                    horizontal = 45.dp,
+//                )
+//        ) {
+//            CustomButton(
+//                text = "Reset Game",
+//                onClick = viewModel::resetGame,
+//                isEnable = viewModel.canResetGame()
+//            )
+//            Spacer(modifier = Modifier.height(10.dp))
+//            CustomButton(
+//                text = "Rules",
+//                onClick = { navController.navigate(Screen.Rules.route) },
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun BoardContent(
+//    onItemSelected: (Int, Int) -> Unit,
+//    boardState: List<List<Player?>>,
+//    isGameOver: Boolean = false,
+//    nextMoveToRemove: Pair<Int, Int>? = null,
+//    viewModel: GameViewModel,
+//    context: Context
+//) {
+//    val color = MaterialTheme.colorScheme
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(
+//                horizontal = 45.dp,
+//            )
+//            .background(
+//                color = color.primary,
+//                shape = RoundedCornerShape(
+//                    15.dp
+//                )
+//            )
+//    ) {
+//        for (row in 0..2) {
+//            val positions = List(3) { col ->
+//                Pair(row, col)
+//            }
+//            BoardRow(
+//                onItemSelected = { selectedPair ->
+//                    Log.i("Board", "Pair $selectedPair")
+//                    onItemSelected(selectedPair.first, selectedPair.second)
+//                },
+//                positions = positions,
+//                rowState = boardState[row],
+//                isGameOver = isGameOver,
+//                nextMoveToRemove = nextMoveToRemove,
+//                viewModel = viewModel,
+//                context = context
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun BoardRow(
+//    onItemSelected: (Pair<Int, Int>) -> Unit,
+//    positions: List<Pair<Int, Int>> = emptyList(),
+//    rowState: List<Player?>,
+//    isGameOver: Boolean,
+//    nextMoveToRemove: Pair<Int, Int>? = null,
+//    viewModel: GameViewModel,
+//    context: Context
+//) {
+//    val soundResources = listOf(
+//        R.raw.sound_1, R.raw.sound_2, R.raw.sound_3,
+//        R.raw.sound_4, R.raw.sound_5, R.raw.sound_6,
+//        R.raw.sound_7, R.raw.sound_8, R.raw.sound_9
+//    )
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(vertical = 5.dp),
+//        horizontalArrangement = Arrangement.SpaceAround
+//    ) {
+//        positions.forEachIndexed { index, pair ->
+//            BoardKeyBox(
+//                onItemSelected = { onItemSelected(pair) },
+//                player = if (rowState[pair.second] == Player.NONE)
+//                    null
+//                else
+//                    rowState[pair.second],
+//                isClickable = !isGameOver,
+//                isNextToRemove = nextMoveToRemove == pair,
+//                soundResId = soundResources[index],
+//                viewModel = viewModel,
+//                context = context
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun BoardKeyBox(
+//    onItemSelected: () -> Unit,
+//    player: Player? = null,
+//    isClickable: Boolean = true,
+//    isNextToRemove: Boolean = false,
+//    soundResId: Int,
+//    viewModel: GameViewModel,
+//    context: Context
+//) {
+//    val color = MaterialTheme.colorScheme
+//    val alpha = if (isNextToRemove) 0.5f else 1f
+//    Box(
+//        modifier = Modifier
+//            .width(80.dp)
+//            .height(80.dp)
+//            .background(
+//                color = color.secondary.copy(alpha = alpha), shape = RoundedCornerShape(
+//                    10.dp
+//                )
+//            )
+//            .clickable(
+//                enabled = player == null && isClickable,
+//                onClick = {
+//                    onItemSelected()
+//                    viewModel.playSound(context, soundResId)
+//                },
+//                indication = rememberRipple(
+//                    bounded = true,
+//                    radius = 70.dp,
+//                    color = color.primary
+//                ),
+//                interactionSource = remember {
+//                    MutableInteractionSource()
+//                }
+//            ),
+//    ) {
+//        Text(
+//            text = when (player) {
+//                Player.PLAYER_1 -> "X"
+//                Player.PLAYER_2 -> "O"
+//                else -> ""
+//            },
+//            style = MaterialTheme.typography.displayMedium.copy(
+//                textAlign = TextAlign.Center
+//            ),
+//            modifier = Modifier
+//                .padding(top = 20.dp, start = 5.dp)
+//                .align(Alignment.Center),
+//            color = when (player) {
+//                Player.PLAYER_1 -> kPlayerXMarkColor
+//                Player.PLAYER_2 -> kPlayerOMarkColor
+//                else -> Color.Transparent
+//            }
+//        )
+//    }
+//}
