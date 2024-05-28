b221ef0a4a690e88d071fe0cad468cb829677bc8
alemoles <alemolerio@gmail.com>
2024-05-27 21:44:52 -0300

    Advanced Tic Tac Toe Jetpack Compose | update rive animation in initial screen

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -6,15 +6,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import app.rive.runtime.kotlin.core.ExperimentalAssetLoader
 import com.devstromo.advancedtictactoe.R
 import com.devstromo.advancedtictactoe.config.helpers.RiveAnimation
 import com.devstromo.advancedtictactoe.domain.GameMode
@@ -22,13 +19,13 @@ import com.devstromo.advancedtictactoe.navigation.Screen
 import com.devstromo.advancedtictactoe.presentation.GameViewModel
 import com.devstromo.advancedtictactoe.presentation.components.CustomButton
 
+@OptIn(ExperimentalAssetLoader::class)
 @Composable
 fun InitialScreen(
     navController: NavController,
     viewModel: GameViewModel,
     modifier: Modifier = Modifier
 ) {
-    val typo = MaterialTheme.typography
     Column(
         modifier = modifier
             .fillMaxSize()
