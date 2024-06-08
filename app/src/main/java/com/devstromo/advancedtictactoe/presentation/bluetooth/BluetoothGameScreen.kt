9cbc36326e74d255f91f67937d86f059ba5d9aeb
alemoles <alemolerio@gmail.com>
2024-06-07 23:03:53 -0300

    Advanced Tic Tac Toe Jetpack Compose | play mode using Bluetooth
    - Create Bluetooth game screen

---
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/bluetooth/BluetoothGameScreen.kt
@@ -0,0 +1,11 @@
+package com.devstromo.advancedtictactoe.presentation.bluetooth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BluetoothGameScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
