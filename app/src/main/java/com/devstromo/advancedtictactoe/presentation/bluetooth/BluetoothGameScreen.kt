734a5e080cc3c46cc78a09e0100cf92820d22af6
alemoles <alemolerio@gmail.com>
2024-06-14 22:20:13 -0300

    Advanced Tic Tac Toe Jetpack Compose | play mode using Bluetooth
    - update BluetoothGameScreen to request bluetooth connection if the service is not turned on
    - code refactor

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/bluetooth/BluetoothGameScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/bluetooth/BluetoothGameScreen.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,8 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
@@ -40,6 +45,7 @@ fun BluetoothGameScreen(
     modifier: Modifier = Modifier,
     viewModel: GameViewModel,
 ) {
+    val typo = MaterialTheme.typography
     val context = LocalContext.current
     var permissionsGranted by remember { mutableStateOf(hasPermissions(context)) }
     var isBluetoothEnabled by remember { mutableStateOf(isBluetoothEnabled()) }
@@ -69,19 +75,39 @@ fun BluetoothGameScreen(
             text = { Text("Bluetooth is required to proceed. Please enable Bluetooth.") },
             confirmButton = {
                 Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(25),
                     onClick = {
                         showEnableBluetoothDialog = false
                         enableBluetoothLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
                     }
                 ) {
-                    Text("Enable")
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(top = 10.dp),
+                        text = "Enable",
+                        textAlign = TextAlign.Center,
+                        style = typo.bodyLarge.copy(color = Color.White)
+                    )
                 }
             },
             dismissButton = {
                 Button(
-                    onClick = { showEnableBluetoothDialog = false }
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(25),
+                    onClick = {
+                        showEnableBluetoothDialog = false
+                    }
                 ) {
-                    Text("Cancel")
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(top = 10.dp),
+                        text = "Cancel",
+                        textAlign = TextAlign.Center,
+                        style = typo.bodyLarge.copy(color = Color.White)
+                    )
                 }
             },
             properties = DialogProperties(dismissOnClickOutside = false)
