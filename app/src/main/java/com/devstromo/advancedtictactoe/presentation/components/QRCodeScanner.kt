6beeb6b69c5ae66ca5057433b8f431e35df2ed8d
alemoles <alemolerio@gmail.com>
2024-06-21 21:37:42 -0300

    Advanced Tic Tac Toe Jetpack Compose | play mode using Bluetooth
    - code refactor

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/QRCodeScanner.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/QRCodeScanner.kt
@@ -4,24 +4,29 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import com.google.zxing.*
+import com.devstromo.advancedtictactoe.presentation.permissions.RequestCameraPermission
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.NotFoundException
+import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.LifecycleOwner
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import com.devstromo.advancedtictactoe.presentation.permissions.RequestCameraPermission
 
 @Composable
 fun QRCodeScanner(onQRCodeScanned: (String) -> Unit) {
