3064153b1b26be81496de05cab7fa684aeefba1a
alemoles <alemolerio@gmail.com>
2024-07-01 22:20:33 -0300

    Advanced Tic Tac Toe Jetpack Compose | create flavors
    - Refactor main menú

---
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/MainScreenMenu.kt
@@ -0,0 +1,9 @@
+package com.devstromo.advancedtictactoe.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreenMenu(modifier: Modifier = Modifier) {
+    
+}
