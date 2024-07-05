205adb955efa7477c72b21350311e0adeb5bdbcc
alemoles <alemolerio@gmail.com>
2024-07-04 23:05:22 -0300

    Advanced Tic Tac Toe Jetpack Compose | create flavors
    - update unit test class

--- app/src/test/java/com/devstromo/advancedtictactoe/presentation/GameViewModelTest.kt
+++ app/src/test/java/com/devstromo/advancedtictactoe/presentation/GameViewModelTest.kt
@@ -1,63 +1,87 @@
 package com.devstromo.advancedtictactoe.presentation
 
 import com.devstromo.advancedtictactoe.domain.Player
+import com.devstromo.advancedtictactoe.domain.online.bluetooth.BluetoothController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 
+@ExperimentalCoroutinesApi
 class GameViewModelTest {
+
+    private lateinit var bluetoothController: BluetoothController
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var testScope: TestScope
+
+    @BeforeEach
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        bluetoothController = mock(BluetoothController::class.java)
+        testScope = TestScope(testDispatcher)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
     @Test
-    fun testCheckForWinnerShouldWinWithNoWin() {
-        val viewModel = GameViewModel()
+    fun testCheckForWinnerShouldWinWithNoWin() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         val board = List(3) { MutableList(3) { Player.NONE } }
         viewModel.updateStateForTesting(board)
         assertFalse(viewModel.checkForWinner(board))
     }
 
     @Test
-    fun testCheckForWinnerShouldWinWithRowWin() {
-        val viewModel = GameViewModel()
+    fun testCheckForWinnerShouldWinWithRowWin() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         val board = listOf(
             mutableListOf(Player.PLAYER_1, Player.PLAYER_1, Player.PLAYER_1),
             mutableListOf(Player.NONE, Player.NONE, Player.NONE),
             mutableListOf(Player.NONE, Player.NONE, Player.NONE)
         )
-        viewModel.updateStateForTesting(
-            board
-        )
+        viewModel.updateStateForTesting(board)
         assertTrue(viewModel.checkForWinner(board))
     }
 
     @Test
-    fun testCheckForWinnerShouldWinWithColumnWin() {
-        val viewModel = GameViewModel()
-        val board =  listOf(
+    fun testCheckForWinnerShouldWinWithColumnWin() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
+        val board = listOf(
             mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
             mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
             mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE)
         )
-        viewModel.updateStateForTesting(
-            board
-        )
+        viewModel.updateStateForTesting(board)
         assertTrue(viewModel.checkForWinner(board))
     }
 
     @Test
-    fun testCheckForWinnerShouldWinWithDiagonal() {
-        val viewModel = GameViewModel()
+    fun testCheckForWinnerShouldWinWithDiagonal() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         val board = listOf(
             mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
             mutableListOf(Player.NONE, Player.PLAYER_1, Player.NONE),
             mutableListOf(Player.NONE, Player.NONE, Player.PLAYER_1)
         )
-        viewModel.updateStateForTesting(
-            board
-        )
+        viewModel.updateStateForTesting(board)
         assertTrue(viewModel.checkForWinner(board))
     }
 
     @Test
-    fun testCheckIfCanResetShouldNotReset() {
-        val viewModel = GameViewModel()
+    fun testCheckIfCanResetShouldNotReset() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         viewModel.updateStateForTesting(
             listOf(
                 mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
@@ -69,8 +93,8 @@ class GameViewModelTest {
     }
 
     @Test
-    fun testCheckIfCanResetShouldReset() {
-        val viewModel = GameViewModel()
+    fun testCheckIfCanResetShouldReset() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         viewModel.updateStateForTesting(
             listOf(
                 mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
@@ -82,8 +106,8 @@ class GameViewModelTest {
     }
 
     @Test
-    fun testResetShouldResetTheBoard() {
-        val viewModel = GameViewModel()
+    fun testResetShouldResetTheBoard() = runTest {
+        val viewModel = GameViewModel(bluetoothController, testDispatcher)
         viewModel.updateStateForTesting(
             listOf(
                 mutableListOf(Player.PLAYER_1, Player.NONE, Player.NONE),
