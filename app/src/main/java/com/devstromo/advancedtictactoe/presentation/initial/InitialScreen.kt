9e680dbeb5a0c0eb278edc39d454e0d34cce5c10
alemoles <alemolerio@gmail.com>
2024-07-16 23:20:40 -0300

    Advanced Tic Tac Toe Jetpack Compose | add language and info in InitialScreen
    - code refactor

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/initial/InitialScreen.kt
@@ -6,14 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import app.rive.runtime.kotlin.core.ExperimentalAssetLoader
@@ -22,7 +20,6 @@ import com.devstromo.advancedtictactoe.config.helpers.RiveAnimation
 import com.devstromo.advancedtictactoe.domain.GameMode
 import com.devstromo.advancedtictactoe.navigation.Screen
 import com.devstromo.advancedtictactoe.presentation.GameViewModel
-import com.devstromo.advancedtictactoe.presentation.components.CustomButton
 import com.devstromo.advancedtictactoe.presentation.components.MainScreenMenu
 
 @OptIn(ExperimentalAssetLoader::class)
