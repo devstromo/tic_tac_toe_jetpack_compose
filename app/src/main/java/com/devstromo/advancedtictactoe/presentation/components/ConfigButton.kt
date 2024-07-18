a9abf0893fa2fda6be84bad4b4e6b0f74c40d755
alemoles <alemolerio@gmail.com>
2024-07-18 16:36:24 -0300

    Advanced Tic Tac Toe Jetpack Compose | add language and info in InitialScreen
    - update config button style

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/ConfigButton.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/ConfigButton.kt
@@ -1,14 +1,22 @@
 package com.devstromo.advancedtictactoe.presentation.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devstromo.advancedtictactoe.ui.theme.AdvancedTicTacToeTheme
@@ -20,19 +28,30 @@ fun ConfigButton(
     onClick: () -> Unit
 ) {
     val typo = MaterialTheme.typography
-    Button(
+    Box(
         modifier = modifier
-            .width(70.dp),
-        shape = RoundedCornerShape(25),
-        border = BorderStroke(2.dp, Color.White),
-        onClick = onClick,
+            .size(70.dp)
+            .border(
+                width = 2.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(25)
+            )
+            .background(Color.Transparent)
+            .clickable(
+                onClick = onClick
+            ),
+        contentAlignment = Alignment.Center
     ) {
         Text(
+            modifier = Modifier
+                .padding(top = 10.dp),
             text = text,
-            style = typo.bodyLarge.copy(
+            style = typo.titleMedium.copy(
                 color = Color.White
             ),
-        )
+            overflow = TextOverflow.Visible,
+
+            )
     }
 }
 
