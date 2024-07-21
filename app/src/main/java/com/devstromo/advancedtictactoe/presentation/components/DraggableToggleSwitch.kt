1c1b12a4b642cb9743abb781d2516df788f400a1
alemoles <alemolerio@gmail.com>
2024-07-20 22:38:39 -0300

    Advanced Tic Tac Toe Jetpack Compose | add language and info in InitialScreen
    - create draggable toggle switch

--- app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/DraggableToggleSwitch.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/presentation/components/DraggableToggleSwitch.kt
@@ -2,6 +2,7 @@ package com.devstromo.advancedtictactoe.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,8 +52,8 @@ fun DraggableToggleSwitch(
                     .fillMaxHeight()
                     .weight(1f)
                     .background(
-                        if (!state.value) MaterialTheme.colorScheme.primary else Color.Transparent,
-                        RoundedCornerShape(20.dp)
+                        color = if (!state.value) MaterialTheme.colorScheme.primary else Color.Transparent,
+                        shape = RoundedCornerShape(20.dp)
                     )
                     .clickable { state.value = false },
                 contentAlignment = Alignment.Center
@@ -66,10 +68,12 @@ fun DraggableToggleSwitch(
                     .fillMaxHeight()
                     .weight(1f)
                     .background(
-                        if (state.value) MaterialTheme.colorScheme.primary else Color.Transparent,
-                        RoundedCornerShape(20.dp)
+                        color = if (state.value) MaterialTheme.colorScheme.primary else Color.Transparent,
+                        shape = RoundedCornerShape(20.dp)
                     )
-                    .clickable { state.value = true },
+                    .clickable(
+                        onClick = { state.value = true },
+                    ),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
