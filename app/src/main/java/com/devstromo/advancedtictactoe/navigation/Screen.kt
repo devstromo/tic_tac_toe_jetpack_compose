da5760146cb559875f6ba26360d6bdab5ede602e
alemoles <alemolerio@gmail.com>
2024-08-03 22:05:07 -0300

    Advanced Tic Tac Toe Jetpack Compose | game infos screen
    - update game app routes

--- app/src/main/java/com/devstromo/advancedtictactoe/navigation/Screen.kt
+++ app/src/main/java/com/devstromo/advancedtictactoe/navigation/Screen.kt
@@ -3,21 +3,26 @@ package com.devstromo.advancedtictactoe.navigation
 import com.devstromo.advancedtictactoe.domain.GameMode
 
 sealed class Screen(val route: String) {
-    data object Initial: Screen("initial_screen")
-    data object Game: Screen("game_screen/{gameMode}") {
+    data object Initial : Screen("initial_screen")
+    data object Game : Screen("game_screen/{gameMode}") {
         fun createRoute(gameMode: GameMode): String {
             return "game_screen/${gameMode.name}"
         }
     }
-    data object Rules: Screen("rules_screen")
 
-    data object Bluetooth: Screen("bluetooth_screen")
+    data object Rules : Screen("rules_screen")
 
-    data object QRScanner: Screen("qr_code_scanner_screen")
+    data object Bluetooth : Screen("bluetooth_screen")
 
-    data object Help: Screen("help_screen")
+    data object QRScanner : Screen("qr_code_scanner_screen")
 
-    data object GameModesInfo: Screen("game_modes_info_screen")
+    data object Help : Screen("help_screen")
 
-    data object GameModeInfo: Screen("game_mode_info_screen/{titleId}/{bodyId}")
+    data object GameModesInfo : Screen("game_modes_info_screen")
+
+    data object GameModeInfo : Screen("game_mode_info_screen/{titleId}/{bodyId}") {
+        fun createRoute(titleId: Int, bodyId: Int): String {
+            return "game_mode_info_screen/${titleId}/${bodyId}"
+        }
+    }
 }
